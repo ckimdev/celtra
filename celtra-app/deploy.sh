781ed8ed9489884e5ad117ce75523846ded8e3cc
chris hyun ho kim <chrishyunhokim@gmail.com>
2017-05-25 17:45:20 -0700

    adding comments

--- celtra-app/deploy.sh
+++ celtra-app/deploy.sh
@@ -7,18 +7,21 @@ log(){
   echo -e "--------------------------------------------------------------------------------\n${NC}"
 }
 
+# Authenticate to AWS to access the docker registry
 log "Authenticating on AWS Container Registry"
 $(aws ecr get-login --region us-west-1)
 
+# Build the docker image and tag it appropriately
 log "Building the docker image"
 docker build -t celtra-app . && \
 docker tag celtra-app:latest 639231722026.dkr.ecr.us-west-1.amazonaws.com/celtra-app:latest
 
+# Push the updated docker image to the remote registry
 log "Pushing the updated docker image to the registry"
 docker push 639231722026.dkr.ecr.us-west-1.amazonaws.com/celtra-app:latest && \
 scp update-containers.sh ec2-user@celtra-app:/home/ec2-user
 
-
+# SSH to the ec2 server and run the update-containers script
 ssh ec2-user@celtra-app << EOF
   /home/ec2-user/update-containers.sh
 EOF
